c5c0ff1af131a32d48a0d66f71ee5f4ccca10c86
BlankDev117 <mmmasent@gmail.com>
2025-06-08 18:25:13 -0400

    Add Missing IGamePadInput port to GamePadStickInput (OSK-17)
    Motivation
    ----
    The current game pad stick input is missing a needed interface for it to be usable as an IGamePadInput
    
    Modifications
    ----
    * Add missing interface
    
    Result
    ----
    The GamePadStickInput is able to behave as expected

--- src/OSK.Inputs/Models/Inputs/GamePadStickInput.cs
+++ src/OSK.Inputs/Models/Inputs/GamePadStickInput.cs
@@ -1,6 +1,6 @@
 ﻿using OSK.Inputs.Models.Configuration;
 
 namespace OSK.Inputs.Models.Inputs;
-public class GamePadStickInput(int id, string name): AnalogInput(id, name, GamePadDevice.DeviceTypeName)
+public class GamePadStickInput(int id, string name): AnalogInput(id, name, GamePadDevice.DeviceTypeName), IGamePadInput
 {
 }
