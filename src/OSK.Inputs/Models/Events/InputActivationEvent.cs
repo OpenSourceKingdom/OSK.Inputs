4f2cbeedf6416e9547c676d17b05a5c61011a0a7
BlankDev117 <mmmasent@gmail.com>
2025-06-11 17:59:54 -0400

    Add UserId to InputActivationEvent (OSK-19)
    Motivation
    ----
    The current InputActivationEvent is lacking context for the user the input being activated and discerning this can be difficult if not impossible
    
    Modifications
    ----
    * Add UserId to InputActivationEvent
    * Set the event user id when the activation context runs the commands generated from input
    
    Result
    ----
    UserId is now available to integrations with the input library

--- src/OSK.Inputs/Models/Events/InputActivationEvent.cs
+++ src/OSK.Inputs/Models/Events/InputActivationEvent.cs
@@ -3,9 +3,11 @@ using OSK.Inputs.Models.Configuration;
 using OSK.Inputs.Models.Runtime;
 
 namespace OSK.Inputs.Models.Events;
-public class InputActivationEvent(IServiceProvider serviceProvider, ActivatedInput input, InputAction inputAction)
+public class InputActivationEvent(IServiceProvider serviceProvider, int userId, ActivatedInput input, InputAction inputAction)
 {
     public IServiceProvider Services => serviceProvider;
+
+    public int UserId => userId;
 
     public ActivatedInput Input => input;
 
