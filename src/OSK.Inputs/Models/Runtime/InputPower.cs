5e4e6cdb35e7c0609f5842b2ba0a41d1fb35506d
BlankDev117 <mmmasent@gmail.com>
2025-06-13 10:38:08 -0400

    Adding some documentation

--- src/OSK.Inputs/Models/Runtime/InputPower.cs
+++ src/OSK.Inputs/Models/Runtime/InputPower.cs
@@ -5,17 +5,37 @@ using System.Linq;
 namespace OSK.Inputs.Models.Runtime;
 
 /// <summary>
-/// Represents the power that a particlar <see cref="ActivatedInput"/> has
+/// Represents the power that a particlar <see cref="ActivatedInput"/> has in anaglog inputs. Power ranges from -1 to 1, where 0 is no power and 1 is full power.
 /// </summary>
 /// <param name="inputPower">The enumerable representing the input power on different axes of a particular input (a joystick has x/y axes for example)</param>
 public class InputPower(IEnumerable<float> inputPower)
 {
     #region Static
 
+    /// <summary>
+    /// An empty input power, representing no power on any axis
+    /// </summary>
     public static InputPower None => new InputPower([]);
 
+    /// <summary>
+    /// Creates an input power with full power on all axes, in the forward direction, where the axis count is specified
+    /// </summary>
+    /// <param name="axisCount">The total number of axes that should be set to full power</param>
+    /// <returns>An input power with all axes set to full</returns>
     public static InputPower FullPower(int axisCount) => new InputPower(Enumerable.Repeat(1f, axisCount));
 
+    /// <summary>
+    /// Creates an input power with full power on all axes, in the reverse direction, where the axis count is specified
+    /// </summary>
+    /// <param name="axisCount">The total number of axes that should be set to full rever power</param>
+    /// <returns>An input power with all axes set to full reverse</returns>
+    public static InputPower ReverseFullPower(int axisCount) => new InputPower(Enumerable.Repeat(-1f, axisCount));
+
+    /// <summary>
+    /// Creates an input power from a set of power levels, where each level corresponds to an axis of the input
+    /// </summary>
+    /// <param name="powerLevels">The set of power levels for each corresponding axis</param>
+    /// <returns>An input power representing all of the axes power levels</returns>
     public static InputPower FromPowerLevels(params float[] powerLevels) => new InputPower(powerLevels);
 
     #endregion
@@ -28,6 +48,23 @@ public class InputPower(IEnumerable<float> inputPower)
 
     #region Helpers
 
+    /// <summary>
+    /// An indexer to access the input power for a specific axis by its index. Short-hand for <see cref="GetAxis(int)"/>
+    /// </summary>
+    /// <param name="index">The specific index to grab input power for</param>
+    /// <returns>The power for the axis</returns>
+    float this[int index] => GetAxis(index);
+
+    /// <summary>
+    /// Retrieves the input power for a specific axis, clamped to -1 to 1.
+    /// 
+    /// <br />
+    /// <br />
+    /// Note: If the index is greater than the number of axes, it will return 0.
+    /// </summary>
+    /// <param name="axisIndex">The axis index to get input power for</param>
+    /// <returns>The power of the axis, clamped between -1 to 1</returns>
+    /// <exception cref="IndexOutOfRangeException">If the axis index is below 0</exception>
     public float GetAxis(int axisIndex)
     {
         if (axisIndex < 0)
--- src/OSK.Inputs/Models/Runtime/PointerInformation.cs
+++ src/OSK.Inputs/Models/Runtime/PointerInformation.cs
@@ -1,25 +1,53 @@
 ﻿using System.Numerics;
 
 namespace OSK.Inputs.Models.Runtime;
+
+/// <summary>
+/// The pointer information for use with an input. This is the location of the input pointer on the screen at the time the input was triggered
+/// </summary>
+/// <param name="pointerId">The specific pointer id. This could vary depending on how many touches or mice are used</param>
+/// <param name="pointerPositions">The set of pointer data for the current pointer id over the course of an input. This data set represents the history of travel, i.e. for swipe related data processing if needed</param>
 public class PointerInformation(int pointerId, Vector2[] pointerPositions)
 {
+    /// <summary>
+    /// A default pointer id, if specific pointer id data is not required
+    /// </summary>
     public const int DefaultPointerId = 999;
 
+    /// <summary>
+    /// Default pointer information for an empty data set of pointer information
+    /// </summary>
     public static PointerInformation Default { get; } = new PointerInformation(DefaultPointerId, []);
 
+    /// <summary>
+    /// The pointer id for the input. This is used to differentiate between multiple pointers, such as in touch or multi-mouse scenarios
+    /// </summary>
     public int PointerId => pointerId;
 
+    /// <summary>
+    /// The current position of the pointer, especailly useful if a data set exists for pointer position hisstory
+    /// </summary>
     public Vector2 CurrentPosition => pointerPositions.Length == 1
         ? pointerPositions[^1]
         : Vector2.Zero;
 
+    /// <summary>
+    /// The previous pointer position, if available, and is useful for calculating deltas or changes in position. 
+    /// This will be the the value of the current position if no previous position exists.
+    /// </summary>
     public Vector2 PreviousPosition => pointerPositions.Length > 1
         ? pointerPositions[^2]
         : CurrentPosition;
 
+    /// <summary>
+    /// The delta between the current and previous pointer positions. This is useful for determining movement or changes in position.
+    /// </summary>
     public Vector2 Delta { get; } = pointerPositions.Length <= 1 
         ? Vector2.Zero 
         : pointerPositions[^1] - pointerPositions[^2];
 
+    /// <summary>
+    /// The list of pointer positions for the current pointer id. This is useful for tracking the history of pointer movements, such as for swipe gestures or other input patterns.
+    /// </summary>
     public Vector2[] PointerPositions => pointerPositions;
 }
