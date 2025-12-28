746e54d28210afb44262a2fab13eaad0433128b5
BlankDev117 <mmmasent@gmail.com>
2025-12-28 15:27:15 -0500

    Change triggers to analog input (OSK-25)
    Motivation
    ----
    GamePad triggers are analog input types, and can receive values between 0 and 1
    
    Modifications
    ----
    * Change the GamePad Triggers from buttons to special Trigger inputs that are analog
    
    Result
    ----
    GamePad inputs for triggers is more accurate to what expected input is

---
+++ src/OSK.Inputs/Models/Inputs/GamePadTriggerInput.cs
@@ -0,0 +1,7 @@
+﻿using OSK.Inputs.Models.Configuration;
+
+namespace OSK.Inputs.Models.Inputs;
+
+public class GamePadTriggerInput(int id, string name) : AnalogInput(id, name, GamePadDevice.DeviceTypeName), IGamePadInput
+{
+}
