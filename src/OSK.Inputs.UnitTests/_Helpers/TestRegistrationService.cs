2044a8771c56ef73383264b332ad5f401307cbb7
BlankDev117 <mmmasent@gmail.com>
2025-12-28 21:32:07 -0500

    Support Registering Service Methods Directly (OSK-28) (#29)
    Motivation
    ----
    The current way of adding action methods to the input system is
    cumbersome and doesn't lend well to large amounts of input methods
    
    Modifications
    ----
    * Added new mechanism to register an entire class worth of methods as
    input actions
    * New format of method expects a return type of `ValueTask` with a
    single parameter for `InputActionEvent`
    * Added new InputActionAttribute to allow customizing action names and
    definitions for an input method
    * Added UnitTests for the new mechanism
    
    Result
    ----
    Library allows adding an entire input action list from a service
    Directly
    
    Fixes #28

---
+++ src/OSK.Inputs.UnitTests/_Helpers/TestRegistrationService.cs
@@ -0,0 +1,37 @@
+﻿using OSK.Inputs.Attributes;
+using OSK.Inputs.Models.Events;
+
+namespace OSK.Inputs.UnitTests._Helpers;
+
+public class TestRegistrationService
+{
+    public ValueTask ValidMethodA(InputActivationEvent activationEvent)
+    {
+        return ValueTask.CompletedTask;
+    }
+
+    [InputAction("SpecialAction")]
+    public ValueTask ValidMethodB(InputActivationEvent activationEvent)
+    {
+        return ValueTask.CompletedTask;
+    }
+
+    public void InvalidMethodA(InputActivationEvent activationEvent) 
+    { 
+    }
+
+    public int InvalidMethodB(InputActivationEvent activationEvent)
+    {
+        return 1;
+    }
+
+    public ValueTask InvalidMethodC()
+    {
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask InvalidMethodD(InputActivationEvent activationEvent, int a)
+    {
+        return ValueTask.CompletedTask;
+    }
+}
---
+++ src/OSK.Inputs/Attributes/InputActionAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OSK.Inputs.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class InputActionAttribute : Attribute
+{
+    #region Variables
+
+    public string? ActionName { get; }
+
+    public string? Description { get; }
+
+    #endregion
+
+    #region Constructors
+
+    public InputActionAttribute(string actionName, string? description = null)
+    {
+        ActionName = actionName?.Trim();
+        Description = description;
+    }
+
+    #endregion
+}
